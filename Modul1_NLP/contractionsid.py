70797893e779569d845d28789ad9f9e414e9d394
emkr-13 <krisna.rata13@gmail.com>
2023-09-22 08:29:05 +0700

    first commit

---
+++ Modul1_NLP/contractionsid.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 01 01:11:02 2016
+
+@author: DIP
+"""
+
+CONTRACTION_MAP = {
+"bkn": "bukan",
+"otw": "sedang dalam perjalanan",
+"oot": "diluar topik",
+"fyi": "sebagai info saja",
+"dg": "dengan",
+"sjk": "sejak",
+"hrga": "harga",
+"pake": "pakai",
+"dr": "dari",
+"tkn": "tekan",
+"utk": "untuk",
+"dptkan": "dapatkan",
+"vchr": "voucher"
+}
